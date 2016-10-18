ba7750672e2b13e7675b1a379b8eeac36374b2e1
jplumbtree87 <jplumbtree87@gmail.com>
2016-10-18 11:35:37 -0400

    added theme and plugin

---
+++ array.php
@@ -0,0 +1,9 @@
+<?php
+
+$aMonths = array("Jan.", "Feb.", "Mar.", "Apr.", "May.", "Jun.");
+
+echo "the 3rd month is " . $aMonths[2];
+
+$aReverse = array("Jan."=>1, "Feb."=>2, "Mar."=>3, "Apr."=>4, "May"=>5, "Jun." =>6);
+
+echo "February is the " . $aReverse["Feb."] . "Month";
